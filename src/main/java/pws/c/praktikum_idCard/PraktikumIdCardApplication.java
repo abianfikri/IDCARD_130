a351242f30fa9edcad98af1a21d1d3c401d2dd3a
abianfikri <abronjoy46@gmail.com>
2022-11-12 12:24:31 +0700

    first  project IDCard

---
+++ src/main/java/pws/c/praktikum_idCard/PraktikumIdCardApplication.java
@@ -0,0 +1,13 @@
+package pws.c.praktikum_idCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumIdCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikumIdCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/c/praktikum_idCard/PraktikumIdCardApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.praktikum_idCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumIdCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
